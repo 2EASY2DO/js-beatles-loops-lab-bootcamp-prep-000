4acf842103bef05f6d8a84f9f4c35dbf37ab6179
Enrique <enriquealexy@gmail.com>
2019-03-04 05:57:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ function theBeatlesPLay(musi,instru){
     console.log (empt);
   }
 }
+
+function johnLennonFacts(facts){
+  var newFacts = [];
+  var i = 0
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!")
+    i++;
+  }
+  return newFacts;
+}
+
+function iLoveTheBeatles(num) {
+  var sentence = [];
+  do {
+    phrases.push("I love the Beatles!");
+    num++
+  } while (num < 15);
+  return sentence;
+}
